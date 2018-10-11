86fb89bd23a4b4f6137bbf9e478f058dd56cd47e
Ishan Tarunesh <ishantarunesh@gmail.com>
2018-10-11 20:43:54 +0530

    Icons

--- website/views.py
+++ website/views.py
@@ -23,25 +23,25 @@ def login_view(request):
 
 	else:
 
-		return render(request, 'website/index.html', {})
+		return render(request, 'website/login.html', {})
 
 def logout_view(request):
 	logout(request)
 	return redirect('login_view')
 
-@login_required()
+#@login_required()
 def home_view(request):
 	user_list = Users.objects.all()
 	return render(request, 'website/home_page.html', {'user_list':user_list})
 
 
-@login_required()
+#@login_required()
 def create_new_resume(request):
 	
 	return render(request, 'website/home_page.html', {})
 
 
-@login_required()
+#@login_required()
 def view_resume(request,id):
 
 	resume = Resume.objects.get(id = id)
@@ -51,7 +51,7 @@ def view_resume(request,id):
 	return render(request, 'website/display_resume.html', {'resume':resume, 'section':list_of_sections, 'point':list_of_points})
 
 
-@login_required()
+#@login_required()
 def view_messages(request):
 	user = 1
 	conv_id = Conversation.objects.filter(Q(user1 = user) | Q(user2 = user))
@@ -59,7 +59,7 @@ def view_messages(request):
 	return render(request, 'website/display_messages.html', {'user':user, 'messages':messages, 'conversations':conv_id})
 
 
-@login_required()
+#@login_required()
 def view_notifications(request):
 	notifications = Notification.objects.all()
 	return render(request, 'website/display_notifications.html', {'notifications':notifications})
